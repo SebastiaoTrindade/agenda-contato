9c7b90f793e93c0b34afa848fa121d3936b300f6
SebastiaoTrindade <sebatrindade@hotmail.com>
2024-10-16 15:41:58 -0300

    Tela Principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :telefones
   resources :enderecos
   resources :contatos
   resources :tipos
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
